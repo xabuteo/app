8567435283de1cec146eb4520c364563fab4794c
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-05-23 08:50:55 +1000

    Update xabuteo.py

--- xabuteo.py
+++ xabuteo.py
@@ -2,7 +2,3 @@ import streamlit as st
 
 st.set_page_config(page_title="Xabuteo", layout="wide", initial_sidebar_state="expanded")
 
-# --- Sidebar Navigation ---
-with st.sidebar:
-    st.markdown("## 📋 Xabuteo Menu")
-    selection = st.radio("Navigate to", list(pages.keys()), label_visibility="collapsed")
