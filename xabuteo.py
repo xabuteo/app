004b148fcd1d5669c69b694c8a4f5642bc109947
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-05-23 08:52:14 +1000

    Update xabuteo.py

--- xabuteo.py
+++ xabuteo.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="Xabuteo", layout="wide", initial_sidebar_state="expanded")
 
+st.title("🏠 Welcome to Xabuteo")
+st.markdown("""Welcome to the world's premier table football online application!""")
