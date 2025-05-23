7921e0617196c84bf760a4d33f4e5051f1336300
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-05-23 10:00:20 +1000

    Update 9_Logout.py

--- pages/9_Logout.py
+++ pages/9_Logout.py
@@ -4,7 +4,6 @@ st.set_page_config(page_title="Logout")
 
 st.title("🚪 Logout")
 
-if st.button("Log out"):
-    st.session_state.clear()
-    st.success("🔒 You have been logged out.")
-    st.rerun()
+st.session_state.clear()
+st.success("🔒 You have been logged out.")
+st.rerun()
