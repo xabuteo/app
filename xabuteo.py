6cc9026e19053e6088a79a593601d6d64ef48122
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-05-28 22:14:27 +1000

    Update xabuteo.py

--- xabuteo.py
+++ xabuteo.py
@@ -23,5 +23,45 @@ if not st.user.is_logged_in:
     # Head to first page of navigation
     pg.run()
 else:
-    st.json(st.user)
+    # Insert into Snowflake (if new)
+    conn = get_snowflake_connection()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(
+            """
+            MERGE INTO xabuteo.public.registrations AS tgt
+            USING (
+                SELECT 
+                    %s AS email,
+                    %s AS first_name,
+                    %s AS last_name,
+                    %s AS auth_id,
+                    CURRENT_TIMESTAMP() AS date_registered,
+                    CURRENT_TIMESTAMP() AS updated_at,
+                    %s AS updated_by
+            ) AS src
+            ON tgt.email = src.email
+            WHEN NOT MATCHED THEN
+                INSERT (email, first_name, last_name, auth0_id, date_registered, updated_at, updated_by)
+                VALUES (src.email, src.first_name, src.last_name, src.auth_id, src.date_registered, src.updated_at, src.updated_by)
+            """,
+            (
+                st.user.email,
+                user_info.get("given_name", ""),
+                user_info.get("family_name", ""),
+                st.user.sub,
+                st.user.email
+            ),
+        )
+        conn.commit()
+    finally:
+        cursor.close()
+        conn.close()    
+    pg = st.navigation(
+        [dashboard_page],
+        position="hidden",
+    )
+    # Head to first page of navigation
+    pg.run()
+    #st.json(st.user)
 
