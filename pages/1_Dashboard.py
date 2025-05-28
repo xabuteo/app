35473f8bcf7d74106996f990c6fcef239ba332ea
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-05-28 21:55:13 +1000

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -2,14 +2,4 @@ import streamlit as st
 
 # Page content
 st.title("📊 Dashboard")
-# Access control
-#if "user_info" not in st.session_state:
-if st.button(
-    "✨ Sign up to the DataFan Store",
-    type="primary",
-    key="checkout-button",
-    use_container_width=True,
-):
-    st.login("auth0")
-#st.success(f"Welcome, {user_name}!")
-st.write("Session state:", dict(st.session_state))
+
