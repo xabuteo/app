ce5d0fd49937c8018d203f43f8c30226fef779e4
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-05-28 21:40:22 +1000

    Update 1_Dashboard.py this time

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -1,14 +1,4 @@
 import streamlit as st
-# from auth import check_auth
-
-#initialize_session()
-#check_auth()
-#st.set_page_config(page_title="Dashboard")
-   
-# Extract user info
-#user_info = st.session_state["user_info"]
-#user_email = user_info.get("email", "Unknown")
-#user_name = user_info.get("name") or f"{user_info.get('given_name', '')} {user_info.get('family_name', '')}".strip() or user_email
 
 # Page content
 st.title("📊 Dashboard")
@@ -20,7 +10,6 @@ if st.button(
     key="checkout-button",
     use_container_width=True,
 ):
-    # st.login("google")
     st.login("auth0")
 #st.success(f"Welcome, {user_name}!")
 st.write("Session state:", dict(st.session_state))
