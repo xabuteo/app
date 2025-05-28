154dc7cb001d0beb1aa0f92892456aaab4bb8848
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-05-28 22:06:50 +1000

    Update xabuteo.py

--- xabuteo.py
+++ xabuteo.py
@@ -14,50 +14,14 @@ dashboard_page = st.Page("./pages/1_Dashboard.py", title="Dashboard", icon=":mat
 profile_page = st.Page("./pages/2_Profile.py", title="Profile", icon=":material/play_arrow:")
 club_page = st.Page("./pages/3_Clubs.py", title="Clubs", icon=":material/admin_panel_settings:")
 
-if st.user.is_logged_in:
-    st.session_state.user_info = user_info
-    st.session_state.user_email = user_info.get("email", "")
-    st.success(f"✅ Logged in as {st.session_state.user_email}")
-
-    # Insert into Snowflake (if new)
-    conn = get_snowflake_connection()
-    cursor = conn.cursor()
-    try:
-        cursor.execute(
-            """
-            MERGE INTO xabuteo.public.registrations AS tgt
-            USING (
-                SELECT 
-                    %s AS email,
-                    %s AS first_name,
-                    %s AS last_name,
-                    %s AS auth_id,
-                    CURRENT_TIMESTAMP() AS date_registered,
-                    CURRENT_TIMESTAMP() AS updated_at,
-                    %s AS updated_by
-            ) AS src
-            ON tgt.email = src.email
-            WHEN NOT MATCHED THEN
-                INSERT (email, first_name, last_name, auth0_id, date_registered, updated_at, updated_by)
-                VALUES (src.email, src.first_name, src.last_name, src.auth_id, src.date_registered, src.updated_at, src.updated_by)
-            """,
-            (
-                st.session_state.user_email,
-                user_info.get("given_name", ""),
-                user_info.get("family_name", ""),
-                user_info.get("sub", ""),
-                user_info.get("email", "")
-            ),
-        )
-        conn.commit()
-    finally:
-        cursor.close()
-        conn.close()
-else:
+if not st.user.is_logged_in:
     # Not yet logged in: show login link and stop
     pg = st.navigation(
         [dashboard_page],
         position="hidden",
     )
-# Head to first page of navigation
-pg.run()
+    # Head to first page of navigation
+    pg.run()
+else:
+    st.json(st.user)
+
