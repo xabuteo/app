6236ecc01cf71a7a47eea6f0ee1fa10bd6d4413d
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-05-28 22:42:50 +1000

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -13,6 +13,9 @@ if not st.user.is_logged_in:
         st.login("auth0")
 else:
     st.success(f"Welcome, {st.user.email}!")
+    st.success(f"Welcome, {st.user.given_name}!")
+    st.success(f"Welcome, {st.user.family_name}!")
+    st.success(f"Welcome, {st.user.sub}!")
     if st.button(
         "✨ Log out",
         type="primary",
