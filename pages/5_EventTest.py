d99c3a3c4be5f5324a984c083928ba9b994d22b1
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-05-30 10:11:29 +1000

    Update 5_EventTest.py

--- pages/5_EventTest.py
+++ pages/5_EventTest.py
@@ -69,7 +69,7 @@ def show():
     
     # Calculate height dynamically
     grid_height = min(row_count, max_rows_to_show) * row_height + header_height
-    grid_height = 449
+    grid_height = 560
     grid_response = AgGrid(
         df_display,
         gridOptions=grid_options,
