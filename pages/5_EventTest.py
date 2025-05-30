e6e083392e8a5742c34adf07706764e869bc90a1
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-05-30 10:02:04 +1000

    Update 5_EventTest.py height 48

--- pages/5_EventTest.py
+++ pages/5_EventTest.py
@@ -64,8 +64,8 @@ def show():
 
     row_count = len(df_display)
     max_rows_to_show = 15
-    row_height = 70  # default row height in pixels
-    header_height = 70
+    row_height = 48  # default row height in pixels
+    header_height = 48
     
     # Calculate height dynamically
     grid_height = min(row_count, max_rows_to_show) * row_height + header_height
