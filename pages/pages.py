29cd7ec6d6fad5b6087a71682f440d2be9e8b6b8
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-06-03 14:02:20 +1000

    Create pages.py

---
+++ pages/pages.py
@@ -0,0 +1,13 @@
+TABS = st.tabs(
+    tabs=[
+        "PROFILE",
+        "CLUBS",
+        "EVENTS",
+    ]
+)
+
+PAGES = [2_profiles, 3_clubs, 4_events]
+
+for t, p in zip(TABS, PAGES):
+    with t:
+        p.page()
