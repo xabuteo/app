0ec585420142d6c80b925799c2b32a6aac4ff36d
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-06-03 15:01:03 +1000

    Update Register.py

--- tabs/Register.py
+++ tabs/Register.py
@@ -4,8 +4,8 @@ from utils import get_snowflake_connection
 
 # in detail.py
 def page(selected_event):
-    st.subheader(f"Event: {event['NAME']}")
-    st.write(f"Starts on: {event['START_DATE']}")
+    st.subheader(f"Event: {selected_event['NAME']}")
+    st.write(f"Starts on: {selected_event['START_DATE']}")
     
     with st.container(border=True):
         # Header and subheader
