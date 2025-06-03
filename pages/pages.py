30959d057bd599feb1f1b289fcb3bf85ff7740c2
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-06-03 14:18:59 +1000

    Update pages.py

--- pages/pages.py
+++ pages/pages.py
@@ -1,16 +1,9 @@
 import streamlit as st
 from tabs import detail, scores, result
 
-TABS = st.tabs(
-    tabs=[
-        "DETAIL",
-        "SCORES",
-        "RESULT",
-    ]
-)
-
+TABS = st.tabs(["DETAIL", "SCORES", "RESULT"])
 PAGES = [detail, scores, result]
 
-for t, p in zip(TABS, PAGES):
-    with t:
-        p.page()
+for tab, page_module in zip(TABS, PAGES):
+    with tab:
+        page_module.page()
