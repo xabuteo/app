09badc8ec78bad5915ffb4c2c7200c317357fc8a
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-06-03 14:37:18 +1000

    Delete pages/pages.py

--- pages/pages.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from tabs import Details, Scores, Result
-
-TABS = st.tabs(["DETAILS", "SCORES", "RESULT"])
-PAGES = [Details, Scores, Result]
-
-for tab, page_module in zip(TABS, PAGES):
-    with tab:
-        page_module.page()
