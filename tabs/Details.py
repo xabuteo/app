4903538559ddb5c469720cf6f6c01c978a8c1650
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-06-03 15:05:38 +1000

    Update Details.py

--- tabs/Details.py
+++ tabs/Details.py
@@ -2,5 +2,5 @@
 import streamlit as st
 
 def page(selected_event):
-    st.subheader(f"Event: {selected_event['NAME']}")
+    st.subheader(f"Event: {selected_event['EVENT_NAME']}")
     st.write(f"Starts on: {selected_event['START_DATE']}")
