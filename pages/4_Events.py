0dfe920c538dbdbfc494ff7a93818a67b979cfaf
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-06-22 21:33:20 +1000

    Update 4_Events.py

--- pages/4_Events.py
+++ pages/4_Events.py
@@ -75,14 +75,15 @@ def show():
     row_count = len(df_display)
     max_rows_to_show = 6
     # style:material 48 & 113 (6 rows) 
-    row_height = 48
-    header_height = 113
+    row_height = 42
+    header_height = 49
     grid_height = min(row_count, max_rows_to_show) * row_height + header_height
 
     grid_response = AgGrid(
         df_display,
         gridOptions=grid_options,
         update_mode=GridUpdateMode.SELECTION_CHANGED,
+        fit_columns_on_grid_load=True,
         enable_enterprise_modules=False,
         height=grid_height,
         theme="alpine"
