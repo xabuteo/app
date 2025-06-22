3134f8cdb79b76cf65010591066d8c0d50353b3e
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-06-22 21:39:21 +1000

    Update 4_Events.py

--- pages/4_Events.py
+++ pages/4_Events.py
@@ -75,7 +75,7 @@ def show():
     row_count = len(df_display)
     max_rows_to_show = 6
     # style:material 48 & 113 (6 rows) 
-    row_height = 48
+    row_height = 42
     header_height = 113
     grid_height = min(row_count, max_rows_to_show) * row_height + header_height
 
