406fd09210ed1b6001470ba35ab260a5c20ad247
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-07-01 18:35:58 +1000

    Update Result.py initial

--- tabs/Result.py
+++ tabs/Result.py
@@ -1,5 +1,47 @@
 import streamlit as st
+import pandas as pd
+from utils import get_snowflake_connection, get_userid
 
 def page(selected_event):
-    st.subheader("Event Results")
-    st.write("Results here.")
+    event_id = selected_event.get("ID")
+
+    try:
+        conn = get_snowflake_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+            SELECT *
+            FROM EVENT_RESULT_V
+            WHERE EVENT_ID = %s
+            ORDER BY order by event_id, competition_type, round_no desc, Final
+        """, (event_id,))
+        rows = cursor.fetchall()
+        cols = [desc[0] for desc in cursor.description]
+        df = pd.DataFrame(rows, columns=cols)
+    except Exception as e:
+        st.error(f"❌ Failed to load final result: {e}")
+        return
+    finally:
+        cursor.close()
+        conn.close()
+
+    if df.empty:
+        st.info("ℹ️ Result has not been finalised.")
+        return
+
+    competitions = sorted(df["COMPETITION_TYPE"].dropna().unique(), key=lambda x: (x != "Open", x))
+
+    for comp in competitions:
+        comp_df = df[df["COMPETITION_TYPE"] == comp][[
+                    "PLAYER", "FINAL_RESULT"
+                ]]
+        with st.expander(f"🏆 {comp} Competition", expanded=(comp == "Open")):
+
+            def highlight_winner(row):
+                style = [''] * len(row)
+                w = row["FINAL_RESULT"]
+                if w = 'Winner':
+                    style[1] = 'background-color: #cce4ff'
+                return style
+
+                styled_df = comp_df.style.apply(highlight_winner, axis=1)
+                st.dataframe(styled_df, use_container_width=True, hide_index=True)
