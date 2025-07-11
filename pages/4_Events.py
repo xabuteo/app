3ca5d0b72725299e40ca1e7fc7c8ba6867e59a68
xabuteo <107371996+xabuteo@users.noreply.github.com>
2025-07-11 18:02:48 +1000

    Update 4_Events.py

--- pages/4_Events.py
+++ pages/4_Events.py
@@ -11,7 +11,13 @@ st.title("📅 Events")
 try:
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM events ORDER BY event_start_date DESC")
+    cursor.execute("SELECT *, TRIM(TRAILING ', ' FROM
+          CASE WHEN event_open    THEN 'Open, '    ELSE '' END ||
+          CASE WHEN event_women   THEN 'Women, '   ELSE '' END ||
+          CASE WHEN event_junior  THEN 'Junior, '  ELSE '' END ||
+          CASE WHEN event_veteran THEN 'Veteran, ' ELSE '' END ||
+          CASE WHEN event_teams   THEN 'Teams, '   ELSE '' END
+    ) AS competitions FROM events ORDER BY event_start_date DESC")
     rows = cursor.fetchall()
     cols = [desc[0] for desc in cursor.description]
     df = pd.DataFrame(rows, columns=cols)
